8fdd7bd72eabef716dd94cdd34123abddf75a16a
metacyber26-source <metacyber26@gmail.com>
2026-06-17 04:14:56 +0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,22 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async headers() {
-    return [
-      {
-        source: '/validation-key.txt',
-        headers: [
-          {
-            key: 'Content-Type',
-            value: 'text/plain; charset=utf-8',
-          },
-          {
-            key: 'Cache-Control',
-            value: 'no-cache, no-store, must-revalidate',
-          },
-        ],
-      },
-    ];
-  },
+  reactStrictMode: true,
 };
 
 export default nextConfig;
